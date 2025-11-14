ff3a4b50ee76f0197dbc99d989788ad18e6fff9f
duris0812 <duris0812@gmail.com>
2025-11-14 13:14:13 -0600

    .
    .

--- proyect/proyect/settings.py
+++ proyect/proyect/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1:5173',
-    'compilator.onrender.com'
+    'https://compilator.onrender.com'
     # Añade aquí el dominio de tu frontend si lo tienes
 ]
 
